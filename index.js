8cdf493bc27ba2dd89e8edc832eabf02a5da832b
StevenMilano <36162914+StevenMilano@users.noreply.github.com>
2020-07-05 10:14:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 let num = 0;
+let arr = null;
+let answer = "";
+let getData = $.get('/csv/japanese-flashcards-hirigana.csv')
+let arrOfWords = [];
+let currentWord = "";
 
+//function that handles and parses csv file with papa.parse
+//when handeling or manipulating the csv, it has to be done in this fucntion
 function numOfWords () {
-    $('.user-input').submit( event => {
-        event.preventDefault();
-        num = $('.user-answer').val();
-        console.log(num);
-    });
-        
-    $.get('/csv/japanese-flashcards-hirigana.csv')
-        .then(data=>{
-            for (let i = num; i < 3; i++) {
-                let result = Papa.parse(data);
-                let randNum = Math.floor(Math.random() * result.data.length);
-                console.log(result.data[randNum]);
+    //recieves amount of words user wants and picks randomly from given csv
+        num = $('.user-num').val();
+        getData.then(data=>{
+            var result = Papa.parse(data);
+                for (let i = 0; i < num; i++) {
+                    let randNum = Math.floor(Math.random() * result.data.length);
+                    arrOfWords = [...arrOfWords, result.data[randNum]]; 
+                    console.log(num);
+                }
+            });
+
+        getData.then(data=>{
+            for (let j = 0; j < arrOfWords.length; j++) {
+                currentWord = arrOfWords[j][0];
+                // if (answer === arrOfWords[j][1]) {
+                //     console.log("correct");
+                // } else {
+                //     console.log("inccorect");
+                // }
             }
-        })
-         
-    
+
+            $('.box').html(`
+                <div class="box">
+                    <h2>${currentWord}</h2>
+                    <form class="user-answer">
+                            <label  for="user-answer" class="screen-reader-text" aria-required="true">Answer</label>
+                            <input class="user-answer" type="text" name="user-answer" id="user-answer" required placeholder="Answer">
+                            <input class="submit-btn" type="submit" value="submit">
+                    </form>
+                </div>
+            `);
+    });
 }
 
-// function displayQuestion () {
-//     for (let i = 0; i < numOfWords(); i++) {
-//         const hiriganaCsv = $.get('/csv/japanese-flashcards-hirigana.csv')
-//         .then(data=>{
-//             let result = Papa.parse(data);
-//             let randNum = Math.floor(Math.random() * result.data.length);
-//             console.log(result.data[randNum]);
-//         })
-//     }
-//     //grab num amount of random vocab words as objects
-//     //access the object with the correct type of vocab
-// }
-
-function isCorrect (answer) {
-
-}
-
-function handleFlashcards() {
+function handleFlashcards(arrOfWords) {
     numOfWords();
-    //displayQuestion();
 }
 
-$(handleFlashcards);
+$('.user-input').on('submit', '.submit-btn', event => {
+    event.preventDefault(); 
+    $(handleFlashcards);
+   // answer = $('.user-answer').val();
+});
