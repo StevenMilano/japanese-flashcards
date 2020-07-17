67b2d197ade29e4b2a52a523d63042e557bea6d5
StevenMilano <36162914+StevenMilano@users.noreply.github.com>
2020-07-17 07:00:07 -0400

    Finished main function

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let i = 0;
 let num = 0;
 let totalQuestions = 0;
 let answer = "";
-let correctAns = 0;
+let currentScore = 0;
+let currentQuestion = 1;
 let show = true;
 let getData = $.get('/csv/japanese-flashcards-hirigana.csv')
 let arrOfWords = [];
@@ -23,8 +24,10 @@ function numOfWords () {
             });
 
         getData.then(data=>{
+            //logs the array of words if you want to look the answers up
             console.log(arrOfWords);
-            $('#current-score').html(`${correctAns}/${num}`);
+            //logs which question you are on
+            $('#current-question').html(`${currentQuestion}/${num}`);
             if (show){
                 $('#display-word').html(`<h2>${arrOfWords[i][0]}</h2>`);
             }
@@ -32,7 +35,8 @@ function numOfWords () {
             $('#user-input').addClass("show").removeClass("hide");           
             $('#display-word').addClass("show").removeClass("hide");
             $('#num-of-words-header').removeClass("show").addClass("hide");
-
+            
+            //This button click is for the submit of an answer while the if else statement checks if the answer is correct or inccorect
             $('.submit-btn').on('click', event =>{
                 event.preventDefault();
                 show = !show;
@@ -44,25 +48,28 @@ function numOfWords () {
                         $('#user-input').removeClass("show").addClass("hide");
                         $('#next').addClass("show").removeClass("hide");
                         $('#user-answer').val('');
-                        correctAns++;
-                        $('#current-score').html(`${correctAns}/${num}`);
+                        currentScore++;
+                        $('#current-question').html(`${currentQuestion}/${num}`);
                     } else if(i < arrOfWords.length && answer != arrOfWords[i][1]) {
                         $('#incorrect').addClass("show").removeClass("hide");
                         $('#display-word').html('');
                         $('#user-input').removeClass("show").addClass("hide");
                         $('#user-answer').removeClass("show").addClass("hide").val('');
                         $('#next').addClass("show").removeClass("hide");
+                        $('#current-question').html(`${currentQuestion}/${num}`);
                         $('#incorrect').html(`
                                 <p>The correct answer is:</p>
                                 <h2>${arrOfWords[i][1]}</h2>
                         `);
                     }  
-
+                    
+                //This watches the next button and moves the user to the next question 
                 $('#next').on('click', event => {
                     event.stopPropagation();
-                    console.log(i, arrOfWords.length);
+                    currentQuestion++;
+                    $('#current-question').html(`${currentQuestion}/${num}`);
                     if (i < arrOfWords.length  - 1){
-                        $('#display-word').addClass("show").removeClass("hide").html(`<h2>${arrOfWords[i][0]}</h2>`);
+                        $('#display-word').addClass("show").removeClass("hide").html(`<h2>${arrOfWords[i + 1][0]}</h2>`);
                         $('#user-input').addClass("show").removeClass("hide");
                         $('#user-answer').addClass("show").removeClass("hide");
                         $('#next').removeClass("show").addClass("hide");
@@ -71,17 +78,35 @@ function numOfWords () {
                         show = !show;
                         i++; 
                     } else if (i === arrOfWords.length - 1) {
-                        console.log("test");
                         $('#incorrect').removeClass("show").addClass("hide");
                         $('#correct').removeClass("show").addClass("hide");
                         $('#final-score').addClass("show").removeClass("hide");
-                        $(this).prop('value', 'Play again');
+                        $('#current-score').addClass("show").removeClass("hide").html(`${currentScore}/${num}`);
+                        $('#current-question').removeClass("show").addClass("hide");
+                        $('#next').removeClass("show").addClass("hide");
+                        $('#playAgain').addClass("show").removeClass('hide');
                         $('#correct').removeClass("show").addClass("hide");
                         $('#incorrect').removeClass("show").addClass("hide");
-                    }
+                    } 
+                    //There when the next button is pressed it goes through this if statement twice for some reason. This prevents that
                     $(this).off(event);
                 });
 
+                $('#playAgain').on('click', event => {
+                    i = 0;
+                    currentScore = 0;
+                    num = 0;
+                    currentQuestion = 1;
+                    $('#display-word').html('');
+                    $('#user-answer').val('');
+                    $('#num-of-words').val('');
+                    $('#current-question').html(`${currentQuestion}/${num}`);
+                    $('#number-input').addClass("show").removeClass("hide");
+                    $('#num-of-words-header').addClass("show").removeClass("hide");            
+                    $('#final-score').removeClass("show").addClass("hide");
+                    $('#current-score').removeClass("show").addClass("hide");
+                    $('#playAgain').removeClass("show").addClass("hide");
+                });
             });
     });
 }
@@ -90,7 +115,7 @@ function numOfWords () {
 function handleFlashcards() {
     numOfWords();
 }
-
+//watches the intial button and executes the function for the rest of the program
 $('#number-input').submit(event => {
     event.preventDefault(); 
     handleFlashcards();
